75557671d4bda3cc4a3f57c12fbdd69945d36a26
Bajax <bajax@bajax.us>
2016-10-10 19:12:40 -0400

    draw/roughing out tools HTML/CSS, JS placeholders created

--- src/routes.js
+++ src/routes.js
@@ -29,5 +29,60 @@ function routes (router)
 			});
 	});
 
+	router.get('/tools_test', (req, res, next) =>
+	{
+		res.render('tools_test', 
+			{
+				title     : req.params.page,
+				page      : req.params.page,
+				primary   : 'rgba(255,255,255,1)',
+				secondary : 'rgba(0,0,0,1)',
+				swatches  : 
+				[
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,0,0,1)',
+					'rgba(0,255,0,1)',
+					'rgba(0,0,255,1)',
+					'rgba(255,255,255,1)',
+					'rgba(255,255,255,1)',
+					'rgba(255,255,255,1)',
+				],
+				layers    : [{id: 1}]
+
+			});
+	});
+
 	return router;
 }
