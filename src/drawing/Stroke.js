305993bd90c5a2f231d0fb57cd58ce7e6f2b306b
Bajax <bajax@bajax.us>
2016-12-16 21:44:38 -0500

    Stroke - passes tests

--- src/drawing/Stroke.js
+++ src/drawing/Stroke.js
@@ -1,45 +1,82 @@
+const EventEmitter = require('events');
 /**
- * Represents a stroke that must be transported by the server.
- * It is a complete stroke-- incomplete strokes are handled differently.
- * Incomplete strokes are an extension that will have to be set up later most likely.
+ * Represents a stroke that must be transported by the server between all the clients.
  */
-
 module.exports = function Stroke (params, p, c)
 {
-	var self = this;
+	const self = this;
 
-	var vertices = [];
-	var tool     = params.tool;
+	var vertices = params.init_vert ? [params.init_vert] : [];
+	var tool     = params.tool || {};
 	var complete = false;
 
-	function receiveVertex(e)
+	/**
+	 * Define all of this object's events in one place, to avoid future bugs 
+	 * eg forgetting to unregister them.
+	 * 0 = which emitter receives the event
+	 * 1 = the event's ID
+	 * 2 = the function to run
+	 */
+	var events = 
+	[
+		[c, c.E.CLEAR_CANVAS, cancel],
+		[c, c.E.DESTRUCT,     cancel],
+		[p, p.E.PEN_MOVE,     receiveVertex],
+		[p, p.E.PEN_UP,       finish],
+		[p, p.E.PEN_CANCEL,   cancel],
+		[p, p.E.TOOL_CHANGE,  cancel],
+		[p, p.E.LEAVE,        cancel],
+	];
+
+	/**
+	 * Receive a new vertex and add it to your list.
+	 */ 
+	function receiveVertex(vertex)
 	{
 		if (!complete)
-			vertices.push([e.x,e.y,e.p,e.t,e.r]);
+			vertices.push(vertex);
 		else
 			throw new Error('client still emitting vertices after stroke completion.');
 	}
 
+	/**
+	 * Stop capturing vertices and discard the stroke.
+	 */
 	function cancel(e)
 	{
 		complete = true;
-		p.removeAll(self); //almost pseudocode for now TODO: make real
-		c.removeAll(self);
+		allOff();
 	}
 
+	/**
+	 * Stop capturing vertices and saves the stroke.
+	 */
 	function finish(e)
 	{
 		complete = true;
-		p.removeAll(self);
-		c.removeAll(self);
-		c.emit(c.E.STROKE_ADD, {tool:tool,vertices:vertices});
+		allOff();
+		c.emit(c.E.STROKE_ADD, {tool:tool, vertices:vertices});
 	}
 
-	p.on(p.E.PEN_MOVE,     receiveVertex);
-	p.on(p.E.PEN_UP,       finish);
-	p.on(p.E.PEN_CANCEL,   cancel);
-	p.on(p.E.TOOL_CHANGE,  cancel);
-	p.on(p.E.LEAVE,        cancel);
-	c.on(c.E.CLEAR_CANVAS, cancel);
-	c.on(c.E.DESTRUCT,     cancel);
+	/**
+	 * Register all events.
+	 */
+	function allOn()
+	{
+		events.forEach(event => event[0].on(event[1], event[2]));
+	}
+
+	/**
+	 * Unregister all events.
+	 */
+	function allOff()
+	{	
+		events.forEach(event => event[0].off(event[1], event[2]));
+	}
+
+	Object.defineProperty(self, 'vertices', {get:()=>vertices});
+	Object.defineProperty(self, 'tool',     {get:()=>tool});
+	Object.defineProperty(self, 'complete', {get:()=>complete});
+
+	allOn();
 }
