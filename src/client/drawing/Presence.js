ea1f30c7779471e09cd265e289dbb5baaf61f73d
Bajax <bajax@bajax.us>
2016-12-16 23:59:07 -0500

    client/Presence - finished roughing out client-side Presence

---
+++ src/client/drawing/Presence.js
@@ -0,0 +1,20 @@
+/**
+ * Represents the presence of a user in a room.  Talks directly to a websocket 
+ * for the individual user.
+ */
+const EVENTS = require('../../shared/EventTypes').PRESENCE;
+
+module.exports = function Presence (params, c)
+{
+	const self = this;
+	
+	var connected = false;
+
+	Object.defineProperty(self, 'connected', { get:()=>connected });
+
+}
+
+module.exports.prototype = 
+{
+	E : EVENTS,
+}
