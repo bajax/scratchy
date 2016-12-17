61aa830f5108ecdfc20a02d1427639ea58f69408
Bajax <bajax@bajax.us>
2016-12-17 14:44:39 -0500

    EventTypes - Formatting/comments

--- src/shared/EventTypes.js
+++ src/shared/EventTypes.js
@@ -3,24 +3,34 @@ module.exports =
 {
 	COORDINATOR : Enum(
 	[
-		'USER_JOIN',
-		'USER_PART',
+		'JOIN',
+		'PART',
 		'STROKE_ADD',
 		'CLEAR_CANVAS',
 		'PUBLISH_HANDLES',
 		'CONSUME_HANDLES',
 		'CONSTRUCT',
-		'DESTRUCT'
+		'DESTRUCT',
 	]),
 	PRESENCE : Enum(
 	[
-		'REPOSITION',
-		'PEN_DOWN',
-		'PEN_MOVE',
-		'PEN_UP',
-		'PEN_CANCEL',
-		'TOOL_CHANGE',
-		'ENTER',
-		'LEAVE'
+		'REPOSITION',    //Relays cursor position information.
+		'PEN_DOWN',      //Pen is placed down.  Relays initial vertex.
+		'PEN_MOVE',      //Pen moves after being placed down.  Relays new vertex.
+		'PEN_UP',        //Pen finishes movement.
+		'PEN_CANCEL',    //Pen stroke is cancelled, and vertices are cleared.
+		'TOOL_CHANGE',   //Relays new tool def.
+		'CLEAR_CANVAS',  //The canvas gets cleared
+		'JOIN',          //A user joins
+		'PART',          //A user parts
+		'KICK',          //The client gets kicked (when someone else gets kicked, it's just a PART)
+	]),
+	TOOL : Enum(
+	[
+		'ACTIVATE',
+		'DEACTIVATE',
+		'DELETE',
+		'COLOR_CHANGE',
+		'CHANGE',
 	]),
 };
