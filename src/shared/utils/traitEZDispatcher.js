229bdbe8505b447423ec14a3535ad6b19b806fe2
Bajax <bajax@bajax.us>
2016-12-29 05:51:14 -0500

    shared/utils/traits - fleshed out
    Does separating code into client/server/shared directories really make sense?

---
+++ src/shared/utils/traitEZDispatcher.js
@@ -0,0 +1,34 @@
+const EventEmitter = require('events');
+
+/**
+ * Adds an on, off, dump etc... methods, adapting to whatever the dispatcher type 
+ * actually is.  Allows emitting and receiving function argument lists of arbitrary
+ * length.
+ * Used so that I can use the same routines on the front-end GUI as I do on the 
+ * backend to manage events.
+ * TODO: Work with more event dispatcher types (i.e. window
+ */ 
+module.exports = function traitEZDispatcher(dispatcher)
+{
+	if (!dispatcher.addEventListener)
+	{
+		const e = new EventEmitter();
+		dispatcher.emit = (...args) => e.emit(...args);
+		dispatcher.on   = (...args) => e.on(...args);
+		dispatcher.off  = (...args) => e.removeListener(...args);
+		dispatcher.dump = (...args) => e.removeAllListeners(...args);
+
+		//stupid but doing it anyway (yay).  May add more later.
+		dispatcher.addEventListener   = (...args) => e.addEventListener(...args);
+		dispatcher.removeListener     = (...args) => e.removeListener(...args);
+		dispatcher.removeAllListeners = (...args) => e.removeAllListeners(...args);
+	}
+	else
+	{
+		//TODO: might need to expand this thing's scope to cover more bases.
+		dispatcher.emit = (...args) => dispatcher.emit(...args);
+		dispatcher.on   = (...args) => dispatcher.on(...args);
+		dispatcher.off  = (...args) => dispatcher.removeListener(...args);
+		dispatcher.dump = (...args) => dispatcher.removeAllListeners(...args);
+	}
+};
--- src/shared/utils/traitResponder.js
+++ src/shared/utils/traitResponder.js
@@ -3,4 +3,22 @@
  */ 
 module.exports = function traitResponder(object, events)
 {
+	/**
+	 * Register all events.
+	 */
+	function allOn()
+	{
+		events.forEach(event => event[0].on(event[1], event[2]));
+	}
+
+	/**
+	 * Unregister all events.
+	 */
+	function allOff()
+	{
+		events.forEach(event => event[0].off(event[1], event[2]));
+	}
+
+	object.allOn  = allOn;
+	object.allOff = allOff;
 };
