daa2fd61b4c2da38f7dbf2e69589c6757fa6bed4
Bajax <bajax@bajax.us>
2017-01-16 10:12:06 -0500

    refactoring server.js (with consts on require)

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
+const reload      = require('reload');
+const http        = require('http');
+const express     = require('express');
+const io          = require('socket.io');
+const body_parser = require('body-parser');
+
 'use strict';
 module.exports = function server(config)
 {
-	const debug       = require('debug')('scratchy:server');
-	const reload      = require('reload');
-	const http        = require('http');
-	const express     = require('express');
-	const app         = express();
-	const io          = require('socket.io');
-	const body_parser = require('body-parser');
-
-	const routes      = require('./routes.js')(config);
-	const sockets     = require('./sockets.js')(config);
-
+	let app         = express();
+	let debug       = require('debug')('scratchy:server');
+	let routes      = require('./routes.js')(config);
+	let sockets     = require('./sockets.js')(config);
 
 	app.set('views', config.dirs.views);
 	app.set('view engine', 'pug');
@@ -23,6 +22,7 @@ module.exports = function server(config)
 	app.use(body_parser.urlencoded({ extended: false }));
 
 	app.use(require('cookie-parser')());
+
 	if (config.env === 'development')
 	{
 		app.use('/css', require('node-sass-middleware')(
@@ -41,15 +41,13 @@ module.exports = function server(config)
 					['babelify', {presets: ['es2015',]} ], 
 				],
 			}));
-	}
-
-	if (config.env === 'development')
 		app.use(express.static(config.dirs.volatile));
-	if (config.env === 'development')
 		app.use(express.static(config.dirs.static));
+	}
 
 	app.use('/', routes(express.Router({strict: true})));
 
+	//404 Handler?  I guess
 	app.use((req, res, next) =>
 	{
 		var err = new Error('Not Found');
