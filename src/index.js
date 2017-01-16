5f321adb7394a114f1b51b101340d76c390000f7
Bajax <bajax@bajax.us>
2017-01-16 10:13:19 -0500

    index.js - now accepts CLI commands
    We gonna be a mini django

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
+#!/usr/bin/env node
+
 //Set NODE_PATH to avoid having to do a bunch of relative imports
 process.env.NODE_PATH = __dirname;
 
-//Just the entry-point for the app
-const config = require('config');
+function filter_params(param, index, array)
+{
+	if (index < 2)
+		return false;
+	if (param[0] === '-')
+		return false;
+	return true;
+}
 
-require('server/server')(config[process.env.deployment_level || 'development']);
+switch (process.argv.filter(filter_params)[0])
+{
+	case undefined:
+	case 'debug':
+	case 'runserver':
+		require('server/server')(require('config')[process.env.deployment_level || 'development']).run();
+		if (process.argv.includes('-b'))
+		{
+			//include the browser runner and start the server's URL
+		}
+		break;
+	case 'test':
+		process.exit()
+		break;
+	default:
+		console.log(`unknown option: '${process.argv[2]}'`)
+		break
+}
+
